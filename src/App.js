0a7ba308dced6e9101207716df8ab59ca8209904
Sunrata Samartha <subrata.samartha2019@gift.edu.in>
2023-04-23 12:56:29 +0530

    sharing screen completed.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import "./styles/main.css";
 import "./styles/room.css";
 import AgoraRTC from "agora-rtc-sdk-ng";
 import { v4 as uuidv4 } from "uuid";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [toggleCameraClass, setToggleCameraClass] = useState(true);
+  const [toggleMicClass, setToggleMicClass] = useState(true);
+  const [toggleScreenClass, setToggleScreenClass] = useState(false);
+
   /* --------------------------------------- agora rtc setup ------------------------------------- */
 
   const APP_ID = "c58def5b2816419bab70004854531f42";
@@ -19,6 +23,9 @@ function App() {
   let localTracks = [];
   let remoteUsers = {};
 
+  let localScreenTracks;
+  let sharingScreen = false;
+
   let joinRoomInit = async () => {
     client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
     await client.join(APP_ID, roomId, token, uid);
@@ -31,7 +38,15 @@ function App() {
   };
 
   let joinStream = async () => {
-    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks();
+    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks(
+      {},
+      {
+        encoderConfig: {
+          width: { min: 640, ideal: 1920, max: 1920 },
+          height: { min: 480, ideal: 1080, max: 1080 },
+        },
+      }
+    );
     let player = `<div class="video__container" id="user-container-${uid}">
                     <div class="video-player" id="user-${uid}"></div>
                   </div>`;
@@ -151,6 +166,100 @@ function App() {
     }
   };
 
+  let toggleCamera = async (e) => {
+    if (localTracks[1].muted) {
+      await localTracks[1].setMuted(false);
+      setToggleCameraClass(true);
+    } else {
+      await localTracks[1].setMuted(true);
+      setToggleCameraClass(false);
+    }
+  };
+
+  let toggleMic = async (e) => {
+    if (localTracks[0].muted) {
+      await localTracks[0].setMuted(false);
+      setToggleMicClass(true);
+    } else {
+      await localTracks[0].setMuted(true);
+      setToggleMicClass(false);
+    }
+  };
+
+  let switchToCamera = async () => {
+    let player = `<div class="video__container" id="user-container-${uid}">
+                    <div class="video-player" id="user-${uid}"></div>
+                  </div>`;
+
+    displayFrame = document.getElementById("stream__box"); //get
+
+    displayFrame.insertAdjacentHTML("beforeend", player);
+
+    await localTracks[0].setMuted(true);
+    await localTracks[1].setMuted(true);
+
+    setToggleMicClass(false);
+    setToggleScreenClass(false);
+
+    localTracks[1].play(`user-${uid}`);
+    await client.publish([localTracks[1]]);
+  };
+
+  let toggleScreen = async (e) => {
+    let cameraButton = document.getElementById("camera-btn");
+    if (!sharingScreen) {
+      sharingScreen = true;
+      setToggleScreenClass(true);
+      setToggleCameraClass(false);
+      cameraButton.style.display = "none";
+
+      localScreenTracks = await AgoraRTC.createScreenVideoTrack();
+
+      console.log(
+        "ckk before remove ",
+        document.getElementById(`user-container-${uid}`)
+      );
+      document.getElementById(`user-container-${uid}`).remove();
+      console.log(
+        "ckk after remove ",
+        document.getElementById(`user-container-${uid}`)
+      );
+
+      displayFrame = document.getElementById("stream__box"); //changes
+
+      displayFrame.style.display = "block";
+      let player = `<div class="video__container" id="user-container-${uid}">
+                  <div class="video-player" id="user-${uid}"></div>
+                </div>`;
+      displayFrame.insertAdjacentHTML("beforeend", player);
+      document
+        .getElementById(`user-container-${uid}`)
+        .addEventListener("click", expandVideoFrame);
+
+      userIdInDisplayFrame = `user-container-${uid}`;
+      localScreenTracks.play(`user-${uid}`);
+
+      await client.unpublish([localTracks[1]]);
+      await client.publish([localScreenTracks]);
+
+      let videoFrames = document.getElementsByClassName("video__container");
+      console.log("samartha", videoFrames);
+      for (let i = 0; i < videoFrames.length; i++) {
+        if (videoFrames[i].id !== userIdInDisplayFrame) {
+          videoFrames[i].style.height = "100px";
+          videoFrames[i].style.width = "100px";
+        }
+      }
+    } else {
+      sharingScreen = false;
+      cameraButton.style.display = "block";
+      document.getElementById(`user-container-${uid}`).remove();
+      await client.unpublish([localScreenTracks]);
+
+      switchToCamera();
+    }
+  };
+
   useEffect(() => {
     addEventListenerToDiv();
   }, []);
@@ -159,6 +268,16 @@ function App() {
     document
       .getElementById("stream__box")
       .addEventListener("click", hideDisplayFrame);
+  }, []);
+
+  useEffect(() => {
+    document
+      .getElementById("camera-btn")
+      .addEventListener("click", toggleCamera);
+    document.getElementById("mic-btn").addEventListener("click", toggleMic);
+    document
+      .getElementById("screen-btn")
+      .addEventListener("click", toggleScreen);
   }, []);
 
   return (
@@ -329,7 +448,9 @@ function App() {
 
             <div id="streams__container"></div>
             <div class="stream__actions">
-              <button>
+              <button
+                id="camera-btn"
+                className={toggleCameraClass ? "active" : ""}>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
@@ -338,7 +459,7 @@ function App() {
                   <path d="M5 4h-3v-1h3v1zm10.93 0l.812 1.219c.743 1.115 1.987 1.781 3.328 1.781h1.93v13h-20v-13h3.93c1.341 0 2.585-.666 3.328-1.781l.812-1.219h5.86zm1.07-2h-8l-1.406 2.109c-.371.557-.995.891-1.664.891h-5.93v17h24v-17h-3.93c-.669 0-1.293-.334-1.664-.891l-1.406-2.109zm-11 8c0-.552-.447-1-1-1s-1 .448-1 1 .447 1 1 1 1-.448 1-1zm7 0c1.654 0 3 1.346 3 3s-1.346 3-3 3-3-1.346-3-3 1.346-3 3-3zm0-2c-2.761 0-5 2.239-5 5s2.239 5 5 5 5-2.239 5-5-2.239-5-5-5z" />
                 </svg>
               </button>
-              <button class="active">
+              <button id="mic-btn" className={toggleMicClass ? "active" : ""}>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
@@ -347,7 +468,9 @@ function App() {
                   <path d="M12 2c1.103 0 2 .897 2 2v7c0 1.103-.897 2-2 2s-2-.897-2-2v-7c0-1.103.897-2 2-2zm0-2c-2.209 0-4 1.791-4 4v7c0 2.209 1.791 4 4 4s4-1.791 4-4v-7c0-2.209-1.791-4-4-4zm8 9v2c0 4.418-3.582 8-8 8s-8-3.582-8-8v-2h2v2c0 3.309 2.691 6 6 6s6-2.691 6-6v-2h2zm-7 13v-2h-2v2h-4v2h10v-2h-4z" />
                 </svg>
               </button>
-              <button>
+              <button
+                id="screen-btn"
+                className={toggleScreenClass ? "active" : ""}>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
@@ -356,7 +479,7 @@ function App() {
                   <path d="M0 1v17h24v-17h-24zm22 15h-20v-13h20v13zm-6.599 4l2.599 3h-12l2.599-3h6.802z" />
                 </svg>
               </button>
-              <button>
+              <button id="leave-btn">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
@@ -513,9 +636,6 @@ function App() {
           </section>
         </div>
       </main>
-      {/* // <div className="App">
-    //   <div className="streams__container" id="streams__container"></div>
-    // </div> */}
     </>
   );
 }
